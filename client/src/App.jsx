efb264065e9483c0061339752827eb95fd3999e8
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-29 09:24:43 +0200

    Changed from BrowserRouter to createBrowserRouter approach

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,28 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import SignupPage from './pages/SignupPage';
-import LoginPage from './pages/LoginPage';
+import { 
+  RouterProvider,
+  createBrowserRouter
+} from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+  {
+    path: "/signin",
+    element: <LoginPage />,
+  },
+]);
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/signin" element={<LoginPage />} />
-      </Routes>
-    </Router>
+      <RouterProvider router={router} />
   );
 }
